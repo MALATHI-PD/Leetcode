b91dc26db34b7e8adae506b97128edfc54028a4f
MALATHI-PD <malathidhandapani25@gmail.com>
2026-08-30 11:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (33.99%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String s) {
+        // char a[]=s.toCharArray();
+        // for(int i=0;i<a.length;i++){
+        //     if(Character.isUpperCase(a[i])){
+        //         a[i]=Character.toLowerCase(a[i]);
+        //     }
+        // }
+        return  s.toLowerCase();
+    }
+}
