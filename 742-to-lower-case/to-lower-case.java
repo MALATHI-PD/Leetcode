7db878e001f4ab1b23d6a9518042a79f9a86939e
MALATHI-PD <malathidhandapani25@gmail.com>
2026-08-30 11:08:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (19.07%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,11 +1,13 @@
 class Solution {
     public String toLowerCase(String s) {
-        // char a[]=s.toCharArray();
-        // for(int i=0;i<a.length;i++){
-        //     if(Character.isUpperCase(a[i])){
-        //         a[i]=Character.toLowerCase(a[i]);
-        //     }
-        // }
-        return  s.toLowerCase();
+        char a[]=s.toCharArray();
+         for(int i=0;i<a.length;i++){
+            int si=a[i];
+            if(si>64 && si<91){
+                a[i]=(char)(si+32);
+            }
+        }
+        return String.valueOf(a);
+        //return  s.toLowerCase();
     }
 }
